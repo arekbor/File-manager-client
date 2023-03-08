078865913cf980347cfdf2816c7ff08d6543f0ad
arekbor <arekindependent@gmail.com>
2023-03-08 18:29:31 +0100

    fix api

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,12 +1,13 @@
 import axios from "axios";
+import * as https from "https";
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
-  httpAgent: {
+  httpAgent: new https.Agent({
     rejectUnauthorized: false,
-  },
+  }),
 });
 
 export default api;
