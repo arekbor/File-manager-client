9570583a7f55300fa3896525403a5cf13a1bd961
arekbor <arekindependent@gmail.com>
2023-06-06 22:43:12 +0200

    fix download loader center

--- src/components/loader/download_loader.tsx
+++ src/components/loader/download_loader.tsx
@@ -1,8 +1,16 @@
+import "./style.css";
+
 const DownloadLoader = () => {
   return (
     <div className="loader-container">
       <div className="spinner"></div>
-      <div className="text-danger">Trwa pobieranie, chwile cierpliwości...</div>
+      <div className="text-spinner text-small-spinner">.·:*¨¨* ≈☆≈ *¨¨*:·.</div>
+      <div className="text-spinner text-danger">
+        Trwa ładowanie pliku, za momencik plik się pobierze...
+      </div>
+      <div className="text-spinner text-small-spinner text-info">
+        Zrób sobię kawę albo herbatę. Ja polecam oczywiście czarną kawę :)
+      </div>
     </div>
   );
 };
